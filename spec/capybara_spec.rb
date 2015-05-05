e3e3d4fc694ce80d5d46a10fce9a1738177f0ad1
thelordofthetimes <thelordofthetimes@gmail.com>
2015-05-05 20:59:54 +0700

    add capybara test

---
+++ spec/capybara_spec.rb
@@ -0,0 +1,5 @@
+RSpec.describe "signin" do
+	it "run" do
+		visit "http://google.com"
+	end
+end
--- spec/mock_spec.rb
+++ spec/mock_spec.rb
@@ -1,1 +1,8 @@
 # add mock spec here
+
+RSpec.describe "mocks" do
+	it("double") do
+		invitation = spy("invitation", {:accept => true});
+		expect(invitation.accept).to eq(true)
+	end
+end
